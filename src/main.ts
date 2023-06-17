caf33264642a3af89723f3b55f05436196460248
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-06-18 00:52:33 +0200

    Attempt at caching builds

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as child_process from "child_process";
+import * as crypto from "crypto";
 import * as fs from "fs";
+import * as os from "os";
 
+import * as cache from "@actions/cache";
 import * as core from "@actions/core";
 
 import { SDL_GIT_URL } from "./constants";
@@ -67,7 +70,7 @@ async function checkout_sdl_git_hash(
 }
 
 async function cmake_configure_build(
-  source_dir: string,
+  SOURCE_DIR: string,
   build_dir: string,
   prefix_dir: string,
   build_type: string,
@@ -77,7 +80,7 @@ async function cmake_configure_build(
     cmake_args = "";
   }
 
-  const configure_command = `cmake -S "${source_dir}" -B ${build_dir} ${cmake_args}`;
+  const configure_command = `cmake -S "${SOURCE_DIR}" -B ${build_dir} ${cmake_args}`;
   const build_command = `cmake --build "${build_dir}" --config ${build_type}`;
   const install_command = `cmake --install "${build_dir}" --prefix ${prefix_dir} --config ${build_type}`;
 
@@ -93,6 +96,46 @@ async function cmake_configure_build(
     core.info(install_command);
     child_process.execSync(install_command, { stdio: "inherit" });
   });
+}
+
+function calculate_state_hash(sdl_git_hash: string) {
+  const ENV_KEYS = [
+    "AR",
+    "CC",
+    "CXX",
+    "ARFLAGS",
+    "CFLAGS",
+    "CXXFLAGS",
+    "LDFLAGS",
+    "CMAKE_PREFIX_PATH",
+    "PKG_CONFIG_PATH",
+  ];
+  const env_state: string[] = [];
+  for (const key of ENV_KEYS) {
+    env_state.push(`${key}=${process.env[key]}`);
+  }
+
+  const ACTION_KEYS = ["build-type", "ninja"];
+  const inputs_state: string[] = [];
+  for (const key of ACTION_KEYS) {
+    const v = core.getInput(key);
+    inputs_state.push(`${key}=${v}`);
+  }
+
+  const misc_state = [`GIT_HASH=${sdl_git_hash}`, `platform=${os.platform()}`];
+
+  const complete_state: string[] = [
+    "ENVIRONMENT",
+    ...env_state,
+    "INPUTS",
+    ...inputs_state,
+    "MISC",
+    ...misc_state,
+  ];
+
+  const state_string = complete_state.join("##");
+
+  return crypto.createHash("sha256").update(state_string).digest("hex");
 }
 
 async function run() {
@@ -153,36 +196,50 @@ async function run() {
     git_branch_hash
   );
 
-  if (USE_NINJA) {
-    await core.group(`Configuring Ninja`, async () => {
-      await configure_ninja_build_tool(SDL_BUILD_PLATFORM);
-    });
-  }
+  const STATE_HASH = calculate_state_hash(git_hash);
+  core.info(`Hash is ${STATE_HASH}`);
 
-  const source_dir = `${SETUP_SDL_ROOT}/src`;
-  const build_dir = `${SETUP_SDL_ROOT}/build`;
-  const install_dir = `${SETUP_SDL_ROOT}`;
-  let cmake_args = `-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}`;
-  if (USE_NINJA) {
-    cmake_args += " -GNinja";
-  }
+  const SOURCE_DIR = `${SETUP_SDL_ROOT}/${STATE_HASH}/source`;
+  const BUILD_DIR = `${SETUP_SDL_ROOT}/${STATE_HASH}/build`;
+  const PACKAGE_DIR = `${SETUP_SDL_ROOT}/${STATE_HASH}/package`;
 
-  await checkout_sdl_git_hash(git_hash, source_dir);
+  await checkout_sdl_git_hash(git_hash, SOURCE_DIR);
 
   const SDL_VERSION =
-    SdlVersion.detect_sdl_version_from_source_tree(source_dir);
+    SdlVersion.detect_sdl_version_from_source_tree(SOURCE_DIR);
   core.info(`SDL version is ${SDL_VERSION.toString()}`);
 
-  await cmake_configure_build(
-    source_dir,
-    build_dir,
-    install_dir,
-    CMAKE_BUILD_TYPE,
-    cmake_args
-  );
+  const CACHE_KEY = `setup-sdl-${STATE_HASH}`;
+  const CACHE_PATHS = [PACKAGE_DIR];
+  const found_cache_key = await cache.restoreCache(CACHE_PATHS, CACHE_KEY, []);
 
-  core.exportVariable(`SDL${SDL_VERSION.major}_ROOT`, install_dir);
-  core.setOutput("prefix", install_dir);
+  if (!found_cache_key) {
+    core.info("No match found in cache. Building SDL from scratch.");
+
+    if (USE_NINJA) {
+      await core.group(`Configuring Ninja`, async () => {
+        await configure_ninja_build_tool(SDL_BUILD_PLATFORM);
+      });
+    }
+
+    let cmake_args = `-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}`;
+    if (USE_NINJA) {
+      cmake_args += " -GNinja";
+    }
+
+    await cmake_configure_build(
+      SOURCE_DIR,
+      BUILD_DIR,
+      PACKAGE_DIR,
+      CMAKE_BUILD_TYPE,
+      cmake_args
+    );
+
+    await cache.saveCache(CACHE_PATHS, CACHE_KEY);
+  }
+
+  core.exportVariable(`SDL${SDL_VERSION.major}_ROOT`, PACKAGE_DIR);
+  core.setOutput("prefix", PACKAGE_DIR);
   core.setOutput("version", SDL_VERSION.toString());
 }
 
